9688767595fe32a65af77fcfdda1b0ca4a147237
JasonCrk <emerzonkolki@gmail.com>
2024-09-23 16:12:22 -0500

    feat(Shared/Domain): HttpResponseException

---
+++ src/Shared/Domain/Exceptions/HttpResponseException.cs
@@ -0,0 +1,8 @@
+namespace Shared.Domain.Exceptions
+{
+    public class HttpResponseException(int statusCode, object? value = null) : Exception
+    {
+        public int StatusCode { get; } = statusCode;
+        public object? Value { get; } = value;
+    }
+}
